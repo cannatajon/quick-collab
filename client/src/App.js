3124d168d15ed0bbea6dabf8d49e58796e974d02
Jon Cannata <joncannats@gmail.com>
2022-05-27 19:07:00 -0400

    text-collab with quill editor

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Editor from './Editor'
+import{ BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import {v4 as uuidV4} from "uuid"
+
+export default function App() {
+  const roomID = uuidV4()
+  console.log(roomID)
+  return (
+    <Router>
+      <Routes>
+        <Route path ='/' exact element={<Navigate to={`/room/${uuidV4()}`}/>}>
+          
+        </Route>
+
+        <Route path ='room/:id' element={<Editor/>}>
+        </Route>
+      </Routes>
+      
+    </Router>
+  )
+}
